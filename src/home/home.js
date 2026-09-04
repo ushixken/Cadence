c5b8a43b70e048a430b72102cfe2f2162b69342a
ushixken <kenushirou@gmail.com>
2026-09-04 10:42:43 +0800

    feat: added function to new-project

---
+++ src/home/home.js
@@ -0,0 +1,9 @@
+const newProject = document.querySelector(".new-project")
+const homePage = document.querySelector(".home-page")
+
+function homeDisappear(){
+  homePage.style.display = "none"
+}
+
+newProject.addEventListener("click", homeDisappear
+)
